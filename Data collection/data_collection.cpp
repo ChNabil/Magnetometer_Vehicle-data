14153e8786df5cb227d74a6ee84d1e31d2704192
Nabil <ch.nabil.nahin@gmail.com>
2017-12-18 23:22:45 -0900

    basic structure

---
+++ Data collection/data_collection.cpp
@@ -0,0 +1,20 @@
+#include "msp430.h"
+void CLOCK_config(void);
+void I2C_config(void);
+void UART_config(void);
+
+void main(void){
+
+}
+
+void CLOCK_config(void){
+
+}
+
+void I2C_config(void){
+
+}
+
+void UART_config(void){
+  
+}
