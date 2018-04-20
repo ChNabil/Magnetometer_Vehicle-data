2c928225cfb170b1cf6856dd356aa5fc4d710f37
Nabil <ch.nabil.nahin@gmail.com>
2018-04-20 05:09:31 -0800

    warning algorithm

--- Data collection/data_collection_new_sensor.cpp
+++ Data collection/data_collection_new_sensor.cpp
@@ -1,4 +1,5 @@
 #include <msp430f5529.h>
+#include <math.h>
 
 void clock_setup();
 void spi_setup();
@@ -21,24 +22,210 @@ char UART_sensor_flag;  // which sensor is sending data via uart
 char j,j1; // for each 100 magnetometer data, 1 distance will be measured. cause magnetometer data fr is 2000Hz, ultrasonic data fr 20Hz
 int sensor_select; // received data from which sensor
 volatile unsigned int i;
-volatile unsigned int a,b,c,d,e,f,g,h,k1,k2;  // for debugging
+//volatile unsigned int a,b,c,d,e,f,g,h,k1,k2;  // for debugging
+volatile int x1, x2, y1, y2, z1, z2, mag1, mag2, mag_th_1, mag_th_2, count1, count2, count3, count4, count5, count6, count7, count8, count9, count10, count11, count12, count13, temp_data_1, temp_data_2, data_check_1, data_check_2;
+int mag_rcnt_1[100];
+int mag_rcnt_2[100];
+volatile int mag1_1000[1000];
+volatile int mag2_1000[1000];
+volatile float speed, length, speed_th, length_th, dist_betwn_snsrs, sampling_rate;
+volatile int store_1000_flag, highest_point_loc_1, highest_point_loc_2, ice_present_flag;
 
 void main()
 {
   WDTCTL = WDTPW + WDTHOLD;
   _EINT();
+
+  dist_betwn_snsrs = 0.125; // in m
+  sampling_rate = 240;
+  ice_present_flag = 0; // if there's ice, flag set to 1
+  length_th = 4.1;  // 4.1m, length of compact car according us epa
+  speed_th = 0.6; // 0.6 m/s, for demonstration, will be changed for later
+
   clock_setup();
   spi_setup();
   ADC_setup();
   UART_setup();
   j = 0;
+  mag_th_1 = 0;
+  mag_th_2 = 0;
+  count1 = 0;   // count first 100 data to calculate threshold
+  store_1000_flag = 0;
 //  P8DIR |= BIT1;    // for debugging
+
+  P1DIR |= BIT0;    // p1.0 led1 / warning
+  P1OUT &= ~BIT0;   // warning off
+
+  P1DIR &= ~ BIT1;  // switch 2, emulate ice
+  P1REN |= BIT1;    // internal resistance enabled
+  P1OUT |= BIT1;    // pull-up resistance
+  P1IFG &= ~BIT1;   // ifg cleared
+  P1IE |= BIT1;     // enable interrupt for switch 2
+
+  P4DIR |= BIT7;    // p4.7 led2 / indicates ice
+  P4OUT &= ~BIT7;   // no ice by default
 
   while(1)
   {
       spi_com1();
       spi_com2();
-      //      distance = 85;  // for debugging
+
+      //    1 set of data is received for 2 magnetometer sensors
+      x1 = (RXData_s1[0]*256)+RXData_s1[1];
+      y1 = (RXData_s1[2]*256)+RXData_s1[3];
+      z1 = (RXData_s1[4]*256)+RXData_s1[5];
+      x2 = (RXData_s2[0]*256)+RXData_s2[1];
+      y2 = (RXData_s2[2]*256)+RXData_s2[3];
+      z2 = (RXData_s2[4]*256)+RXData_s2[5];
+      mag1 = sqrt((x1*x1) + (y1*y1) + (z1*z1));
+      mag2 = sqrt((x2*x2) + (y2*y2) + (z2*z2));
+
+      if(count1<100)    // storing first 100 data and calculating threshold
+          {
+          mag_rcnt_1[count1] = mag1; // store 100 recent mag
+          mag_rcnt_2[count1] = mag2;
+          count1++;
+          mag_th_1 += mag1;
+          mag_th_2 += mag2;
+          if(count1 == 100) // calculating threshold for first 100 data
+                {
+                    mag_th_1 = mag_th_1/100;
+                    mag_th_2 = mag_th_2/100;
+                }
+          }
+
+      else
+      {
+          for (count2=0;count2<99;count2++) // shifting 99 data points to the left to make room for most recent data at 100th place
+          {
+              mag_rcnt_1[count2] = mag_rcnt_1[count2+1];
+              mag_rcnt_2[count2] = mag_rcnt_2[count2+1];
+          }
+          mag_rcnt_1[99] = mag1;    // add the most recent mag value to the array of recent 100 values
+          mag_rcnt_2[99] = mag2;
+          count3 = 0; // to check if last 10 data exceed threshold
+          if (store_1000_flag == 0) // if this flag is 1, skip this stage and start storing
+              {
+              for (count11=90;count11<100;count11++)
+          {
+              if((mag_rcnt_1[count11]-mag_th_1) > 3 || (mag_rcnt_1[count11]-mag_th_1) < -3)
+                  count3++;
+          }
+          if (count3 == 10)
+          {
+              store_1000_flag = 1;
+              count4 = 100;
+              for(count12=0;count12<100;count12++)  // storing first 100 data of 1000
+              {
+                  mag1_1000[count12] = mag_rcnt_1[count12];
+                  mag2_1000[count12] = mag_rcnt_2[count12];
+              }
+          }
+              }
+      }
+
+      if (store_1000_flag == 1 && count4<1000)
+      {
+          mag1_1000[count4] = mag1;
+          mag2_1000[count4] = mag2;
+          count4++;
+      }
+
+      if(count4 == 1000 && store_1000_flag == 1)    // 1000 data stored, time for calculation
+      {
+          P1OUT &= ~BIT0;   // warning sign off
+          store_1000_flag = 0; // stop storing data and start calculations. will start checking recent 10 mag after calculations are done
+
+// median filter for removing the spikes
+          for(count10=1;count10<999;count10++)
+          {
+              if(mag1_1000[count10] > mag1_1000[count10-1] && mag1_1000[count10] > mag1_1000[count10+1])
+                  mag1_1000[count10] = mag1_1000[count10+1];
+              else if(mag1_1000[count10] < mag1_1000[count10-1] && mag1_1000[count10] < mag1_1000[count10+1])
+                  mag1_1000[count10] = mag1_1000[count10+1];
+
+              if(mag2_1000[count10] > mag2_1000[count10-1] && mag2_1000[count10] > mag2_1000[count10+1])
+                  mag2_1000[count10] = mag2_1000[count10+1];
+              else if(mag2_1000[count10] < mag2_1000[count10-1] && mag2_1000[count10] < mag2_1000[count10+1])
+                  mag2_1000[count10] = mag2_1000[count10+1];
+          }
+
+//  moving avg filter. doesnt seem to be very useful
+
+//          for(count6=0;count6<981;count6++) // moving avg filter, only valid first 900 data
+//          {
+//              temp_data_1 = 0;
+//              temp_data_2 = 0;
+//              for(count5=0;count5<20;count5++)
+//              {
+//                  temp_data_1 += mag1_1000[count6+count5];
+//                  temp_data_2 += mag2_1000[count6+count5];
+//              }
+//              mag1_1000[count6] = temp_data_1/20;
+//              mag2_1000[count6] = temp_data_2/20;
+//          }
+
+          // removing mag threshold so that squaring wont cause any prob, works like removing mean but dont have to calculate mean
+          for(count8=0;count8<1000;count8++)
+          {
+              mag1_1000[count8] = mag1_1000[count8] - mag_th_1;
+              mag2_1000[count8] = mag2_1000[count8] - mag_th_2;
+              mag1_1000[count8] = mag1_1000[count8] * mag1_1000[count8];
+              mag2_1000[count8] = mag2_1000[count8] * mag2_1000[count8];
+          }
+
+          // finding time difference between 2 sensors data
+          data_check_1 = mag1_1000[0];
+          highest_point_loc_1 = 0;
+          data_check_2 = mag2_1000[0];
+          highest_point_loc_2 = 0;
+          for(count7=1;count7<1000;count7++)
+          {
+              if(mag1_1000[count7]>data_check_1)
+              {
+                  data_check_1 = mag1_1000[count7]; // highest point of the graph
+                  highest_point_loc_1 = count7;  // index of the highest point
+              }
+              if(mag2_1000[count7]>data_check_2)
+              {
+                  data_check_2 = mag2_1000[count7];
+                  highest_point_loc_2 = count7;
+              }
+          }
+// get wrong index sometimes, need to check [fixed]
+
+
+          speed = dist_betwn_snsrs/((highest_point_loc_2 - highest_point_loc_1)/sampling_rate);   // speed in m/s
+
+// finding length. We know max point is "highest_point_loc_1", check when the mag becomes same as threshold after that.
+// since we are removing threshold, need to find when mag becomes close to 0. checking 2 consecutive samples to be sure.
+// stopping the for loop when we find that point
+          for(count9=highest_point_loc_1;count9<999;count9++)
+          {
+              if(mag1_1000[count9]<10 && mag1_1000[count9+1]<10)
+              {
+                  length = (count9 - 89)/sampling_rate;    //89+20=109, 89 data before detecting car, 20 for moving avg filter(not using)
+                  length = length * speed;  //   length in m
+                  count9 = 1000;    // stopping the for loop right here
+              }
+          }
+
+          if(ice_present_flag == 1) // if ice present, threshold speed reduced by 50%
+              speed_th = speed_th/2;
+
+          if(length > length_th)    // if length of the car is longer than avg
+              speed_th = speed_th - (speed_th * ((int(length - length_th)) * 0.1));   // 10% reduction in speed threshold for each meter increase in length from avg length
+
+          if(speed > speed_th)
+          {
+              P1OUT |= BIT0;    // warning sign on, will remain on until another car detected
+          }
+
+      }
+
+
+      //////////////////////////////////////////////////////////
+
       j++;
 //      P8OUT ^= BIT1;    // for debugging
       if (j==12){
@@ -367,4 +554,33 @@ void __attribute__ ((interrupt(USCI_A1_VECTOR))) USCI_A1_ISR (void)
   break;                             // Vector 4 - TXIFG
   default: break;
   }
+}
+
+// Port 1 interrupt service routine
+
+#pragma vector=PORT1_VECTOR
+
+__interrupt void Port_1(void)
+
+{
+
+    switch( __even_in_range( P1IV, P1IV_P1IFG7 )) {
+
+    case P1IV_P1IFG1:   // Pin 1.1 (switch 2)
+        if(ice_present_flag == 0)
+        {
+            ice_present_flag = 1;   // ice present
+            P4OUT |= BIT7;  // ice led on
+        }
+        else
+        {
+            ice_present_flag = 0;   // if pressed again, ice not present
+            P4OUT &= ~BIT7; // ice led off
+        }
+        break;
+
+    default:   _never_executed();
+
+
+    }
 }
